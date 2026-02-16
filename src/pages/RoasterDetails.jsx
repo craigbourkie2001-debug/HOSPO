698553eb10fa4ceebe38da23714e16bd4bfff03f
Craig Bourke <craig.bourkie2001@gmail.com>
2026-02-16 18:05:24 +0000

    File changes

--- src/pages/RoasterDetails.jsx
+++ src/pages/RoasterDetails.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { base44 } from "@/api/base44Client";
-import { useQuery, useMutation, useQueryClient } from "@tantml:react-query";
+import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
 import { Input } from "@/components/ui/input";
